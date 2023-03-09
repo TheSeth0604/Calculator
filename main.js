881ae37aac6eac83e2b7e3177282cf0163574e83
TheSeth0604 <alvarocrispin0604@gmail.com>
2023-03-09 12:06:00 -0500

    js basics

---
+++ main.js
@@ -0,0 +1,4 @@
+const number1 = document.querySelector('.button-number');
+const number2 = document.querySelector('.button-number');
+const operation = document.querySelector('.button-operation');
+const result = document.querySelector('.button-result');
